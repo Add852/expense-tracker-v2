3b7b862ee314b32451857c3ee19c1a88c98f14a4
Add852 <xadd852x@gmail.com>
2024-11-06 18:32:46 +0800

    color palette dashboard progress

--- dashboard/index.php
+++ dashboard/index.php
@@ -15,33 +15,66 @@
     include "../components/navbar.html";
     ?>
 
-    <main class='pt-16 h-screen bg-gray-800'>
+    <main class='test pt-16 h-screen bgGreen'>
         <!-- BALANCE CONTAINER AND DIVIDER COL -->
-
-
-        <div class='border border-one border-gray-700 bg-gray-800 fixed w-full h-20 py-2 px-5 md:px-10 lg:px-20 xl:'>
+        <div class='border border-one border-gray-700 bgGreen fixed w-full h-20 py-2 px-5 md:px-10 lg:px-20 xl:'>
             <!-- BALANCE CONTAINER -->
-            <div class='border border-one border-gray-700 bg-gray-900 rounded-xl w-full h-full items'>
+            <div class='border border-one border-gray-700 tlGreen rounded-3xl w-full h-full items'>
                 <!-- BALANCE and ADD BUTTON ROW-->
                 <div class="flex flex-grow px-5 h-full items-center">
                     <!-- BALANCE and ADD ROW-->
                     <div class="flex flex-row gap w-full">
-                        <p class="text-gray-100 text-2xl">Balance:</p>
-                        <p class='text-gray-100 text-2xl'>$12,000.00</p>
+                        <p class="text-gray-300 text-2xl">Balance:</p>
+                        <p class='text-gray-300 text-2xl'>$12,000.00</p>
                     </div>
-                    <button class='bg-gray-300 rounded-xl h-1/2 px-5'>Add</button>
+                    <!-- ADD BUTTON -->
+                    <button class='text-gray-300 btGreen rounded-3xl h-1/2 px-5'>Add</button>
                 </div>
             </div>
         </div>
 
         <!-- PANELS -->
-        <div class='bg-gray-900 pt-24 pb-12 px-5 overflow-y-scroll grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 sm:px-10 md:px-16 lg:px-20 xl:px-24 xl:px-30'>
+        <div class='bgGreen pt-24 pb-12 px-5 overflow-y-scroll grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 sm:px-10 md:px-16 lg:px-20 xl:px-24 xl:px-30'>
             <?php
             for ($i = 0; $i < 5; $i++) {
-                echo "<div class='border border-one border-gray-700 bg-gray-800 hover:bg-gray-700 duration-150 rounded-xl w-full h-64'></div>";
+                echo "<div class='border border-one border-gray-700 tlGreen hover:bg-gray-700 duration-150 rounded-3xl w-full h-64'></div>";
             }
             ?>
         </div>
+
+        <style>
+            /* background colors */
+            .bgGreen {
+                background-color: #03352c;
+            }
+
+            .tlGreen {
+                background-color: #072822;
+            }
+
+            .btGreen {
+                background-color: #1a664d;
+            }
+
+            /* TAILWIND VALUES:
+            TEXT COLORS
+                - Default: text-gray-300
+                - Hyperlinks: text-teal-300
+
+            MISCELLANEOUS
+                - Panel/Tiles
+                    - border: rounded-3xl
+                - Textboxes
+                    - Corner: rounded-lg
+                    - Border: border-one (1px)
+                    - Border Color: border-gray-400
+                    - Transparency: bg-transparent
+                    - Remove Outline: focus:outline-none
+                - Buttons
+                    - Border: rounded-3xl
+            */
+        </style>
+
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -7,11 +7,44 @@
     <title>Xpense Tracker</title>
 </head>
 
-<body class="bg-gray-900">
+<body class="bgGreen">
     <?php
     include_once "components/navbar.html";
     ?>
     <p class="pt-16 text-gray-100">DITO LANDING PAGE NI KITTT</p>
+
+    <style>
+        /* background colors */
+        .bgGreen {
+            background-color: #03352c;
+        }
+
+        .tlGreen {
+            background-color: #072822;
+        }
+
+        .btGreen {
+            background-color: #1a664d;
+        }
+
+        /* TAILWIND VALUES:
+            TEXT COLORS
+                - Default: bg-gray-300
+                - Hyperlinks: text-teal-300
+
+            MISCELLANEOUS
+                - Panel/Tiles
+                    - border: rounded-3xl
+                - Textboxes
+                    - Corner: rounded-lg
+                    - Border: border-one (1px)
+                    - Border Color: border-gray-400
+                    - Transparency: bg-transparent
+                    - Remove Outline: focus:outline-none
+                - Buttons
+                    - Border: rounded-3xl
+            */
+    </style>
 </body>
 
 </html>
