91baa891eb2193038466d06121d5da6ec9e69263
Add852 <xadd852x@gmail.com>
2024-11-07 14:43:45 +0800

    404 page

--- router.php
+++ router.php
@@ -10,4 +10,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 if (array_key_exists($uri, $routes)) {
     require($routes[$uri]);
+} else {
+    http_response_code(404);
+    require('views/404.php');
 }
---
+++ views/404.php
@@ -0,0 +1,7 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/navbar.php') ?>
+
+<p class="pt-16 textGray font-bold ">Page Not Found</p>
+<a href="/" class="textTeal hover:underline">Go back..</a>
+
+<?php require('partials/footer.php') ?>
