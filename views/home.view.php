a44dff6ec514e2fb89b64b7218e4478bac0194ea
kit <keithrussel632@gmail.com>
2024-11-07 22:45:57 +0800

    site

--- views/home.view.php
+++ views/home.view.php
@@ -3,4 +3,18 @@
 
 <p class="pt-16 text-teal-300">DITO LANDING PAGE NI KITTT</p>
 
+<div class="container">
+    <div>
+        <div>
+            <div>
+                <section>
+                    <div>
+                        <h1>Track your expenses</h1>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php require('partials/footer.php') ?>
