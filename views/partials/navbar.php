05ff5248ad1f096b493cc7b1b61ba69745940b04
knmnltzy <knmnltzy@gmail.com>
2024-11-08 23:26:12 +0800

    working user drop down menu

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -5,7 +5,7 @@
         <div>
             <!-- Image Logo -->
             <a href='/'>
-                <img class="h-8 w-auto" src="https://th.bing.com/th/id/OIP.wvOQEvpiAyv7s2_c_2yG-QHaHa?w=187&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                <img class="h-10 w-auto" src="https://th.bing.com/th/id/OIP.wvOQEvpiAyv7s2_c_2yG-QHaHa?w=187&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7"
                     alt="Your Company">
             </a>
         </div>
@@ -20,21 +20,34 @@
             <div>
                 <button type="button"
                     class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                    id="toggleButton" aria-expanded=" false" aria-haspopup="true">
                     <span class="absolute -inset-1.5"></span>
                     <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full"
+                    <img class="h-10 w-auto rounded-full"
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt="">
                 </button>
             </div>
-            <div class="absolute z-50 right-0 mt-2 w-48 origin-top-right rounded-md py-2 tlGreen shadow-lg border border-gray-800">
-                <a href="#" class="hover:bg-emerald-900 block px-4 py-2 text-sm text-gray-300">Your Profile</a>
+            <div id="targetDiv" class="hidden absolute z-50 right-0 mt-2 w-48 origin-top-right rounded-md py-2 tlGreen shadow-lg border border-gray-800">
+                <a href="/userSettings" class="hover:bg-emerald-900 block px-4 py-2 text-sm text-gray-300">Your Profile</a>
                 <a href="#" class="hover:bg-emerald-900 block px-4 py-2 text-sm text-gray-300">Settings</a>
                 <a href="#" class="hover:bg-emerald-900 block px-4 py-2 text-sm text-gray-300">Sign out</a>
             </div>
         </div>
     </div>
-
-
 </nav>
+<script>
+    const button = document.querySelector("#toggleButton");
+    const targetDiv = document.querySelector("#targetDiv");
+
+    button.addEventListener("click", function(event) {
+        event.stopPropagation();
+        targetDiv.classList.toggle("hidden");
+    });
+
+    document.addEventListener("click", function(event) {
+        if (!targetDiv.contains(event.target) && event.target !== button) {
+            targetDiv.classList.add("hidden");
+        }
+    });
+</script>
