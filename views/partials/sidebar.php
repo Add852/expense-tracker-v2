622bb8a4551c16c713ea7228d6ca3cc308381583
Add852 <xadd852x@gmail.com>
2024-11-09 13:05:20 +0800

    goal button

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -7,6 +7,7 @@
         </svg>
     </a>
 
+
     <!-- HOME ICON -->
     <a href="/dashboard" title="Dashboard" class="p-3 hover:bg-emerald-900">
         <svg class="textGray" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
@@ -28,6 +29,13 @@
         <svg class="textGray" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 0 1 8.25-8.25.75.75 0 0 1 .75.75v6.75H18a.75.75 0 0 1 .75.75 8.25 8.25 0 0 1-16.5 0Z" clip-rule="evenodd" />
             <path fill-rule="evenodd" d="M12.75 3a.75.75 0 0 1 .75-.75 8.25 8.25 0 0 1 8.25 8.25.75.75 0 0 1-.75.75h-7.5a.75.75 0 0 1-.75-.75V3Z" clip-rule="evenodd" />
+        </svg>
+    </a>
+
+    <!-- GOAL -->
+    <a href="/goal" title="Dashboard" class="p-3 hover:bg-emerald-900">
+        <svg class="textGray" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <path fill-rule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clip-rule="evenodd" />
         </svg>
     </a>
 
