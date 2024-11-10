e280af6451652b0313358e3901a0696142ac7076
knmnltzy <knmnltzy@gmail.com>
2024-11-10 22:34:20 +0800

    bring back goal

--- views/goal.view.php
+++ views/goal.view.php
@@ -1,6 +1,6 @@
 <?php require('partials/head.php') ?>
 
-<main class='h-screen bgGreen'>
+<main class='test h-screen bgGreen'>
     <!-- Username -->
     <h2 class="px-2 py-0 sm:text-3xl lg:text-4xl font-bold text-gray-300 flex justify-center items-center my-2 lg:px-2">USERNAME'S GOALS</h2>
     <!-- Sidebar -->
@@ -22,21 +22,20 @@
             </div>
         </div>
 
-        <!-- Paayos nito bagu i-uncomment, sinasakop kasi to ung sidebar, also unsuble toh :) -->
-        <!-- Main Content Area --> 
-        <div class="tlGreen rounded-lg">
-            <!-- <?php
-            // echo "
-            //     <button class='px-4 py-4 my-4 btGreen text-xl w-full font-semibold text-gray-300 border-gray-400 rounded-3xl transition duration-150 hover:bg-gray-700 text-center'>
-            //         Add Goal Button
-            //     </button>";
-            // for ($i = 1; $i <= 3; $i++) {
-            //     echo "
-            //     <button class='px-4 py-4 my-4 btGreen text-xl w-full font-semibold text-gray-300 border-gray-400 rounded-3xl transition duration-150 hover:bg-gray-700 text-center'>
-            //         Goal Listed Goated Fanum Tax KEKW Sigma Ohio Rizz Ligma Balls Madge Sadge 
-            //     </button>";
-            // }
-            ?> --> 
+        <!-- Main Content Area -->
+        <div class="flex-grow tlGreen rounded-lg h-full p-4 text-gray-300 border-gray-400 col-span-3 sm:mx-4 sm:mt-2 lg:mt-2 mr-4">
+            <?php
+            echo "
+                <button class='px-4 py-4 my-4 btGreen text-xl w-full font-semibold text-gray-300 border-gray-400 rounded-3xl transition duration-150 hover:bg-gray-700 text-center'>
+                    Add Goal Button
+                </button>";
+            for ($i = 1; $i <= 3; $i++) {
+                echo "
+                <button class='px-4 py-4 my-4 btGreen text-xl w-full font-semibold text-gray-300 border-gray-400 rounded-3xl transition duration-150 hover:bg-gray-700 text-center'>
+                    Goal Listed Goated Fanum Tax KEKW Sigma Ohio Rizz Ligma Balls Madge Sadge 
+                </button>";
+            }
+            ?>
         </div>
     </div>
 </main>
