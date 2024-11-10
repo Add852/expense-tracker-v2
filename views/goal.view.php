0d7de2898a9b6bdc8e3f438a4290d4b91a01fe10
Raymond Roman <raymond.g.roman10@gmail.com>
2024-11-10 14:08:44 +0800

    um gitna ko nalang username goals

--- views/goal.view.php
+++ views/goal.view.php
@@ -4,10 +4,11 @@
     <!--  -->
     <?php require('partials/balance.php') ?>
 
+    <!-- Username -->
+    <h2 class="px-2 py-0 sm:text-3xl lg:text-4xl font-bold text-gray-300 flex justify-center items-center my-2 lg:px-2">USERNAME'S GOALS</h2>
     <!-- Sidebar -->
     <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-4">
         <div class="bgGreen pb-12 px-5 flex flex-col lg:space-y-4 md:w-full pr-4">
-        <h2 class="px-2 py-0 text-2xl font-bold text-gray-300 flex justify-center items-center my-2 lg:px-2">USERNAME'S GOALS</h2>
             <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-1">
                 <button class="px-4 py-6 btGreen text-xl font-semibold text-gray-300 border-gray-400 rounded-3xl transition duration-150 hover:bg-gray-700 text-center">
                     >> Current Goals
