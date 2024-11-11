5a5c46bc07754d01cd85674ebcf95fd956667cd4
LowelleJade <jadelearning2004@gmail.com>
2024-11-11 17:41:53 +0800

    login page update

--- views/login.view.php
+++ views/login.view.php
@@ -1,57 +1,65 @@
-<?php require('partials/head.php') ?>
+<?php require('partials/headNoSide.php') ?>
 
 <div class="bgGreen min-h-screen">
 
-    <div class="flex items-center justify-center min-h-screen">
+    <div class="grid sm:grid-cols-1 md:grid-cols-2 h-screen text-center items-center">
 
-        <div class="h-screen flex text-center items-center justify-center">
+        <div class="hidden md:inline">
+            <h1 class="text-9xl text-gray-300 font-semibold">XPENSE</h1>
+        </div>
 
-            <div class="rounded-3xl tlGreen p-8">
+        <!-- Ayaw opacity kapag ginamit yung tlGreen -->
 
-                <h2 class="text-4xl font-semibold text-center textGray">Account Login</h2>
+        <div class="bg-green-950 rounded-3xl md:mx-16 p-8 bg-opacity-0 sm:mx-4 md:bg-opacity-100">
+
+        <!-- <div class="tlGreen rounded-3xl md:mx-16 p-8 bg-opacity-0 sm:mx-4 md:bg-opacity-100"> -->
+
+            <h2 class="text-4xl font-semibold text-center textGray">Account Login</h2>
+            <br>
+
+            <hr class="border-gray-300 my-4">
+
+            <form method="post" action="" class="space-y-1">
+
+                <label for="user"></label>
                 <br>
 
-                <hr class="border-gray-300 my-4">
-
-                <form method="post" action="" class="space-y-1">
-
-                    <label for="user"></label>
-                    <br>
-
-                    <input
-                        type="username"
-                        id="loginusername"
-                        placeholder="Username"
-                        class="w-full p-3 border border-gray-400 textGray bg-transparent rounded-lg focus:outline-none"
-                        required>
-                    <br>
-
-                    <label for="pass"></label>
-                    <br>
-
-                    <input
-                        type="password"
-                        id="loginpassword"
-                        placeholder="Password"
-                        class="w-full p-3 border border-gray-400 textGray bg-transparent rounded-lg focus:outline-none"
-                        required>
-                    <br> <br>
-
-                    <button
-                        type="submit"
-                        class="w-full py-3 mt-4 text-xl font-semibold textGray btGreen rounded-3xl hover:bgGreen">Login
-                    </button>
-
-                </form>
-
+                <input
+                    type="username"
+                    id="loginusername"
+                    placeholder="Username"
+                    class="w-full p-3 border border-gray-400 textGray bg-transparent rounded-lg focus:outline-none"
+                    required>
                 <br>
-                <hr class="border-gray-300 my-4">
 
-                <h3 class="text-xl text-gray-300">No account yet?
-                    <a href="/signup" class="textTeal hover:underline">Sign Up</a>
-                </h3>
+                <label for="pass"></label>
+                <br>
 
-            </div>
+                <input
+                    type="password"
+                    id="loginpassword"
+                    placeholder="Password"
+                    class="w-full p-3 border border-gray-400 textGray bg-transparent rounded-lg focus:outline-none"
+                    required>
+                <br> <br>
+
+                <button
+                    type="submit"
+                    class="w-full py-3 mt-4 text-xl font-semibold textGray btGreen rounded-3xl hover:bgGreen">Login
+                </button>
+
+            </form>
+
+            <br>
+
+            <a href="/ForgetPass.html" class="textTeal hover:underline">Forgot password</a>
+
+            <br>
+            <hr class="border-gray-300 my-4">
+
+            <p class="text-xl text-gray-300">No account yet?
+                <a href="/SignUp.php" class="textTeal hover:underline">Sign Up</a>
+            </p>
 
         </div>
 
