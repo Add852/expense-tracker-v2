c6c5494657976cec91cf391dab8bd34549c5cd8f
knmnltzy <knmnltzy@gmail.com>
2024-11-13 11:23:42 +0800

    collapseable sm:non-collapsable

--- views/add.view.php
+++ views/add.view.php
@@ -1,100 +1,98 @@
 <?php require('partials/head.php') ?>
 
-<main id="view">
-    <div id="panel">
-        <div class="form-buttons">
-            <button id="btn1" class="btn" type="button" onclick="showForm('1')">
-                Group
-            </button>
-            <button id="btn2" class="btn activeBtn" type="button" onclick="showForm('2')">
-                Basic
-            </button>
-            <button id="btn3" class="btn" type="button" onclick="showForm('3')">
-                Subscription
-            </button>
-        </div>
-        <form>
-            <div id="form1" class="form-container">
-                <label for="groups">Choose a Group</label>
-                <select id="groups" name="groups">
-                    <option value="" disabled selected>Select here</option>
-                    <option value="option1">Group 1</option>
-                    <option value="option2">Group 2</option>
-                    <option value="option3">Group 3</option>
-                </select><br>
-                <label for="amountG">Amount</label>
-                <input type="number" id="amountG" name="amountG"><br>
-                <label for="categoryG">Choose a Category</label>
-                <select id="categoryG" name="categoryG">
-                    <option value="" disabled selected>Select here</option>
-                    <option value="option1">Slavery</option>
-                    <option value="option2">Food</option>
-                    <option value="option3">Travel</option>
-                </select><br>
-                <label for="descG">Description</label>
-                <input type="text" id="descG" name="descG"><br>
-                <label for="datetimeG">Expense Date and Time</label>
-                <input type="datetime-local" id="datetimeG" name="datetimeG" required><br>
-                <button class="btnSubmit" type="submit">Submit Group Expense</button>
-            </div>
-        </form>
-        <form>
-            <div id="form2" class="form-container activeForm">
-                <label for="amountB">Amount</label>
-                <input type="number" id="amountB" name="amountB"><br>
-                <label for="categoryB">Choose a Category</label>
-                <select id="categoryB" name="categoryB">
-                    <option value="" disabled selected>Select here</option>
-                    <option value="option1">Slavery</option>
-                    <option value="option2">Food</option>
-                    <option value="option3">Travel</option>
-                </select><br>
-                <label for="descB">Description</label>
-                <input type="text" id="descB" name="descB"><br>
-                <label for="datetimeB">Expense Date and Time</label>
-                <input type="datetime-local" id="datetimeB" name="datetimeB" required><br>
-                <button class="btnSubmit" type="submit">Submit Basic Expense</button>
-            </div>
-        </form>
-        <form>
-            <div id="form3" class="form-container">
-                <label for="amountS">Amount</label>
-                <input type="number" id="amountS" name="amountS"><br>
-                <label for="period">Subscription Plan</label>
-                <select id="period" name="period">
-                    <option value="" disabled selected>Select here</option>
-                    <option value="option1">Weekly</option>
-                    <option value="option2">Monthly</option>
-                    <option value="option3">Yearly</option>
-                </select><br>
-                <label for="categoryS">Choose a Category</label>
-                <select id="categoryS" name="categoryS">
-                    <option value="" disabled selected>Select here</option>
-                    <option value="option1">Slavery</option>
-                    <option value="option2">Food</option>
-                    <option value="option3">Travel</option>
-                </select><br>
-                <label for="descS">Description</label>
-                <input type="text" id="descS" name="descS"><br>
-                <label for="datetimeS">Expense Date and Time</label>
-                <input type="datetime-local" id="datetimeS" name="datetimeS" required><br>
-                <button class="btnSubmit" type="submit">Submit Form 3</button>
-            </div>
-        </form>
+<main>
+    <div class=" form-buttons">
+        <button id="btn1" class="btn  bg-emerald-700  " type="button" onclick="showForm('2')">
+            Basic
+        </button>
+        <button id="btn2" class="btn btGreen" type="button" onclick="showForm('3')">
+            Subscription
+        </button>
+        <button id="btn3" class="btn btGreen" type="button" onclick="showForm('1')">
+            Group
+        </button>
     </div>
+
+    <form id="form1">
+        <div class="grid">
+            <label for="amountB">Amount</label>
+            <input type="number" id="amountB" name="amountB">
+            <label for="categoryB">Choose a Category</label>
+            <select id="categoryB" name="categoryB">
+                <option value="" disabled selected>Select here</option>
+                <option value="option1">Slavery</option>
+                <option value="option2">Food</option>
+                <option value="option3">Travel</option>
+            </select>
+            <label for="descB">Description</label>
+            <input type="text" id="descB" name="descB">
+            <label for="datetimeB">Expense Date and Time</label>
+            <input type="datetime-local" id="datetimeB" name="datetimeB" required>
+            <button class="btGreen" type="submit">Submit Basic Expense</button>
+        </div>
+    </form>
+    <form id="form2" class="hidden">
+        <div class="grid">
+            <label for="amountS">Amount</label>
+            <input type="number" id="amountS" name="amountS">
+            <label for="period">Subscription Plan</label>
+            <select id="period" name="period">
+                <option value="" disabled selected>Select here</option>
+                <option value="option1">Weekly</option>
+                <option value="option2">Monthly</option>
+                <option value="option3">Yearly</option>
+            </select>
+            <label for="categoryS">Choose a Category</label>
+            <select id="categoryS" name="categoryS">
+                <option value="" disabled selected>Select here</option>
+                <option value="option1">Slavery</option>
+                <option value="option2">Food</option>
+                <option value="option3">Travel</option>
+            </select>
+            <label for="descS">Description</label>
+            <input type="text" id="descS" name="descS">
+            <label for="datetimeS">Expense Date and Time</label>
+            <input type="datetime-local" id="datetimeS" name="datetimeS" required>
+            <button class="btnSubmit" type="submit">Submit Form 3</button>
+        </div>
+    </form>
+    <form id="form3" class="hidden">
+        <div class="grid">
+            <label for="groups">Choose a Group</label>
+            <select id="groups" name="groups">
+                <option value="" disabled selected>Select here</option>
+                <option value="option1">Group 1</option>
+                <option value="option2">Group 2</option>
+                <option value="option3">Group 3</option>
+            </select>
+            <label for="amountG">Amount</label>
+            <input type="number" id="amountG" name="amountG">
+            <label for="categoryG">Choose a Category</label>
+            <select id="categoryG" name="categoryG">
+                <option value="" disabled selected>Select here</option>
+                <option value="option1">Slavery</option>
+                <option value="option2">Food</option>
+                <option value="option3">Travel</option>
+            </select>
+            <label for="descG">Description</label>
+            <input type="text" id="descG" name="descG">
+            <label for="datetimeG">Expense Date and Time</label>
+            <input type="datetime-local" id="datetimeG" name="datetimeG" required>
+            <button class="btnSubmit" type="submit">Submit Group Expense</button>
+        </div>
+    </form>
 </main>
 <script>
     function showForm(id) {
-        document.querySelectorAll('.form-container').forEach(form => {
-            form.classList.remove('activeForm');
-        });
+
 
         document.querySelectorAll('.btn').forEach(button => {
-            button.classList.remove('activeBtn');
+            button.classList.remove('bg-emerald-700');
         });
-
-        document.getElementById('form' + id).classList.add('activeForm');
-        document.getElementById('btn' + id).classList.add('activeBtn');
+        document.getElementById('form1').classList.add('grid');
+        document.getElementById('form1').classList.add('grid');
+        document.getElementById('form' + id).classList.add('grid');
+        document.getElementById('btn' + id).classList.add('bg-emerald-700');
     }
 
     function setCurrentDateTime(inputId) {
--- views/partials/head.php
+++ views/partials/head.php
@@ -7,32 +7,33 @@
     <title>Expense Tracker</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/forms@0.4.0/dist/forms.min.js"></script>
     <style>
-            .bgGreen {
-                background-color: #03352c;
-            }
+        .bgGreen {
+            background-color: #03352c;
+        }
 
-            .tlGreen {
-                background-color: #072822;
-            }
+        .tlGreen {
+            background-color: #072822;
+        }
 
-            .btGreen {
-                background-color: #1a664d;
-                color: white;
-            }
+        .btGreen {
+            background-color: #1a664d;
+            color: white;
+        }
 
 
-            .textGray {
-                /* same as text-gray-300 (DEFAULT TEXT COL)*/
-                color: #d1d5db;
-            }
+        .textGray {
+            /* same as text-gray-300 (DEFAULT TEXT COL)*/
+            color: #d1d5db;
+        }
 
-            .textTeal {
-                /* same as text-teal-300 (FOR HYPERLINKS) */
-                color: #5eead4;
-            }
+        .textTeal {
+            /* same as text-teal-300 (FOR HYPERLINKS) */
+            color: #5eead4;
+        }
 
-            /* TAILWIND VALUES:
+        /* TAILWIND VALUES:
         MISCELLANEOUS
             - Panel/Tiles
                 - border: rounded-3xl
@@ -46,65 +47,7 @@
                 - Border: rounded-3xl
                 - hover:bg-emerald-700
         */
-            /* Temporary Styles */
-            .btnSubmit {
-                justify-self: center;
-                background-color: greenyellow;
-                padding: 3px 6px;
-            }
-
-            .btnSubmit:hover {
-                background-color: violet;
-            }
-
-            .form-container {
-                display: none;
-                margin-top: 20px;
-            }
-
-            .activeForm {
-                display: grid;
-            }
-
-
-            .btn {
-                margin-right: 10px;
-                padding: 10px;
-                background-color: #4CAF50;
-                color: white;
-                border: none;
-                cursor: pointer;
-            }
-
-            .btn:hover {
-                background-color: #45a049;
-            }
-
-            .activeBtn {
-                background-color: hotpink;
-            }
-
-            .form-buttons {
-                margin-bottom: 5px;
-            }
-
-            #panel {
-                margin-top: 20px;
-                justify-content: center;
-                align-items: center;
-                padding: 20px 30px;
-                background-color: teal;
-                display: grid;
-            }
-
-            #view {
-                width: 100%;
-                height: 90vh;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                background-color: red;
-            }
+        /* Temporary Styles */
     </style>
 </head>
 
