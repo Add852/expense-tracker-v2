1e2ea1284a2ba0216fccb97ef2ac6d05f6130903
knmnltzy <knmnltzy@gmail.com>
2024-11-13 22:45:00 +0800

    add expense mobile view

--- views/add.view.php
+++ views/add.view.php
@@ -1,113 +1,133 @@
 <?php require('partials/head.php') ?>
+<style>
+    select option:hover {
+        background-color: rgb(4, 120, 87);
+    }
+</style>
 <main class="flex justify-center items-center sm:justify-start">
-    <div class=" text-gray-200">
-        <div class="form-buttons space-x-1 text-md sm:text-xl font-semibold my-4">
-            <button id="btn1" class="btn bg-gray-200 text-gray-700 px-4 py-1 rounded-3xl" type="button" onclick="showForm('1')">
+
+    </form>
+    <div class="text-gray-200">
+        <div class="form-buttons space-x-1 text-lg sm:text-xl font-semibold my-4">
+            <button
+                id="btn1"
+                class="btn px-4 py-1 rounded-3xl btGreen bg-gray-200 text-gray-700"
+                type="button"
+                onclick="showForm('1')"
+                disabled>
                 Basic
             </button>
-            <button id="btn2" class="btn btGreen px-4 py-1 rounded-3xl" type="button" onclick="showForm('2')">
+            <button
+                id="btn2"
+                class="btn px-4 py-1 rounded-3xl btGreen hover:bg-emerald-700 hover:text-white active:scale-95"
+                type="button"
+                onclick="showForm('2')">
                 Subscription
             </button>
-            <button id="btn3" class="btn btGreen px-4 py-1 rounded-3xl" type="button" onclick="showForm('3')">
+            <button
+                id="btn3"
+                class="btn px-4 py-1 rounded-3xl btGreen hover:bg-emerald-700 hover:text-white active:scale-95"
+                type="button"
+                onclick="showForm('3')">
                 Group
             </button>
         </div>
 
-        <form id="form1">
+        <form>
             <div class="grid space-y-5">
-                <input type="number" id="amountB" name="amountB" placeholder="Amount" class="p-1 pl-2 rounded-lg border border-gray-400 bg-transparent focus:outline-none">
-                <select id="categoryB" name="categoryB" class="p-1 rounded-lg border border-gray-400 bgGreen focus:outline-none">
-                    <option value="" disabled selected>Choose a Category</option>
-                    <option value="option1">Slavery</option>
-                    <option value="option2">Food</option>
-                    <option value="option3">Travel</option>
-                </select>
-                <input type="text" id="descB" name="descB" placeholder="Description" class="p-1 rounded-lg border border-gray-400 bg-transparent focus:outline-none">
-                <input type="datetime-local" id="datetimeB" name="datetimeB" required class="p-1 rounded-lg border border-gray-400 bg-transparent focus:outline-none">
-                <button class="btGreen" type="submit" class="w-full py-3 mt-4 text-xl font-semibold textGray btGreen rounded-3xl hover:bgGreen">Submit Basic Expense</button>
-            </div>
-        </form>
-        <form id="form2" class="hidden">
-            <div class="grid">
-                <label for="amountS">Amount</label>
-                <input type="number" id="amountS" name="amountS">
-                <label for="period">Subscription Plan</label>
-                <select id="period" name="period">
-                    <option value="" disabled selected>Select here</option>
+                <input
+                    type="number"
+                    id="amount"
+                    name="amount"
+                    placeholder="Amount"
+                    required
+                    class="p-1 pl-2 rounded-lg border border-gray-400 bgGreen focus:outline-none">
+                <select
+                    id="form2"
+                    name="period"
+                    required
+                    class="p-1 rounded-lg border border-gray-400 bgGreen focus:outline-none hidden">
+                    <option value="" disabled selected>Subscription Plan</option>
                     <option value="option1">Weekly</option>
                     <option value="option2">Monthly</option>
                     <option value="option3">Yearly</option>
                 </select>
-                <label for="categoryS">Choose a Category</label>
-                <select id="categoryS" name="categoryS">
-                    <option value="" disabled selected>Select here</option>
-                    <option value="option1">Slavery</option>
-                    <option value="option2">Food</option>
-                    <option value="option3">Travel</option>
-                </select>
-                <label for="descS">Description</label>
-                <input type="text" id="descS" name="descS">
-                <label for="datetimeS">Expense Date and Time</label>
-                <input type="datetime-local" id="datetimeS" name="datetimeS" required>
-                <button class="btnSubmit" type="submit">Submit Form 3</button>
-            </div>
-        </form>
-        <form id="form3" class="hidden">
-            <div class="grid">
-                <label for="amountG">Amount</label>
-                <input type="number" id="amountG" name="amountG">
-                <label for="groups">Choose a Group</label>
-                <select id="groups" name="groups">
-                    <option value="" disabled selected>Select here</option>
+                <select
+                    id="form3"
+                    name="group"
+                    required
+                    class="p-1 rounded-lg border border-gray-400 bgGreen focus:outline-none hidden">
+                    <option value="" disabled selected>Group</option>
                     <option value="option1">Group 1</option>
                     <option value="option2">Group 2</option>
                     <option value="option3">Group 3</option>
                 </select>
-                <label for="categoryG">Choose a Category</label>
-                <select id="categoryG" name="categoryG">
-                    <option value="" disabled selected>Select here</option>
+                <select
+                    id="category"
+                    name="category"
+                    required
+                    class="p-1 rounded-lg border border-gray-400 bgGreen focus:outline-none">
+                    <option value="" selected disabled>Category</option>
                     <option value="option1">Slavery</option>
                     <option value="option2">Food</option>
                     <option value="option3">Travel</option>
                 </select>
-                <label for="descG">Description</label>
-                <input type="text" id="descG" name="descG">
-                <label for="datetimeG">Expense Date and Time</label>
-                <input type="datetime-local" id="datetimeG" name="datetimeG" required>
-                <button class="btnSubmit" type="submit">Submit Group Expense</button>
+                <input
+                    type="text"
+                    id="desc"
+                    name="desc"
+                    placeholder="Description"
+                    required
+                    class="p-1 rounded-lg border border-gray-400 bgGreen focus:outline-none">
+                <input
+                    type="datetime-local"
+                    id="datetime"
+                    name="datetime"
+                    required
+                    class="p-1 rounded-lg border border-gray-400 bgGreen focus:outline-none">
+                <input
+                    type="hidden"
+                    id="type"
+                    name="type"
+                    value="basic">
+                <button
+                    class="px-4 py-1 text-lg sm:text-xl font-semibold btGreen rounded-3xl hover:bg-emerald-700 active:scale-95"
+                    type="submit">
+                    Add Expense
+                </button>
             </div>
         </form>
-        <script>
-            function showForm(id) {
-                document.querySelectorAll('.btn').forEach(button => {
-                    button.classList.remove('bg-emerald-700');
-                });
-                document.getElementById('form1').classList.add('hidden');
-                document.getElementById('form2').classList.add('hidden');
-                document.getElementById('form3').classList.add('hidden');
-                document.getElementById('form' + id).classList.remove('hidden');
-                document.getElementById('btn' + id).classList.add('bg-emerald-700');
-            }
 
-            function setCurrentDateTime(inputId) {
-                const now = new Date();
-                const year = now.getFullYear();
-                const month = String(now.getMonth() + 1).padStart(2, '0');
-                const day = String(now.getDate()).padStart(2, '0');
-                const hours = String(now.getHours()).padStart(2, '0');
-                const minutes = String(now.getMinutes()).padStart(2, '0');
-
-                const formattedDateTime = `${year}-${month}-${day}T${hours}:${minutes}`;
-                document.getElementById(inputId).value = formattedDateTime;
-            }
-
-            window.onload = function() {
-                setCurrentDateTime('datetimeG');
-                setCurrentDateTime('datetimeB');
-                setCurrentDateTime('datetimeS');
-            };
-        </script>
     </div>
 </main>
+<script>
+    function showForm(id) {
+        document.querySelectorAll('.btn').forEach(btn => {
+            btn.classList.remove('bg-gray-200', 'text-gray-700', 'hover:bg-emerald-700', 'hover:text-white', 'active:scale-95');
+            btn.classList.add('hover:bg-emerald-700', 'hover:text-white', 'active:scale-95');
+            btn.disabled = false;
+        });
+        document.getElementById('btn' + id).classList.add('bg-gray-200', 'text-gray-700');
+        document.getElementById('btn' + id).classList.remove('hover:bg-emerald-700', 'hover:text-white', 'active:scale-95');
+        document.getElementById('btn' + id).disabled = true;
+
+
+        document.getElementById('form2').classList.add('hidden');
+        document.getElementById('form3').classList.add('hidden');
+        document.getElementById('form' + id)?.classList.remove('hidden');
+    }
+
+    window.onload = function() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+
+        const formattedDateTime = `${year}-${month}-${day}T${hours}:${minutes}`;
+        document.getElementById('datetime').value = formattedDateTime;
+    };
+</script>
 
 <?php require('partials/footer.php') ?>
