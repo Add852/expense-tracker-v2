d3afd93de3847f06e0995e32ad7e22624f40669f
knmnltzy <knmnltzy@gmail.com>
2024-11-22 16:41:29 +0800

    justify end in balance

--- views/partials/balance.php
+++ views/partials/balance.php
@@ -9,7 +9,8 @@ $_SESSION['balance'] = $balance;
 ?>
 
 <!-- BALANCE ICON -->
-<div class="bg-emerald-900 text-base sm:text-xl rounded-xl h-8 flex justify-between items-center pl-1 pr-2">
+
+<div class="bg-emerald-900 text-base sm:text-xl rounded-xl h-8 flex justify-end items-center pl-1 pr-2">
     <button id="addBalanceButton" class="h-6 w-6 mr-3">
         <svg class="text-gray-300 hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
