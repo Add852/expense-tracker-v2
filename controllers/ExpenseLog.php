13142a08afeb2ece0a9d888ab8e4d7a27b1dfcc3
Add852 <xadd852x@gmail.com>
2024-11-22 11:11:03 +0800

    dateformat function, expenselog db connection, and fixed naming convention

--- controllers/ExpenseLog.php
+++ controllers/ExpenseLog.php
@@ -4,8 +4,11 @@
 protectPage();
 // Use $_SESSION['userid']; to get logged in user's userid
 
+//current user
+$userID = $_SESSION['userid'];
+//fetch all the current user's expenses
+$expenses = $db->query('select expenses.* from users join expenses on users.userID=expenses.userID where users.userID=?;', [$userID])->fetchAll(PDO::FETCH_ASSOC);
+
 $title = 'Expenses Log';
 
-require('views/ExpenseLog.php');
-
-?>
+require('views/expenselog.view.php');
---
+++ views/expenselog.view.php
@@ -0,0 +1,149 @@
+<?php require('partials/head.php') ?>
+
+<div class="container mx-auto p-4">
+    <div>
+        <!-- mobile View -->
+        <!-- yung magiging default css netong mga to nasa javascrtipt. sundan nalng per ID -->
+        <div id="mobile_view" class="hidden" onclick="toggleView()">
+            <h2 class="text-xl font-bold text-white bg-green-700 p-2  rounded-t-lg" id="amount "> ₱ 35 </h2>
+            <div class="flex justify-between items-center  rounded-t-lg p-2  ">
+                <!--Concatenate nalang dito ung Title saka yung value -->
+                <p class="text-sm text-gray-300 " id="category"> Category: food </p>
+                <p class="text-sm text-gray-300 " id="expense_time"> 11 Sep 2001 </p>
+            </div>
+        </div>
+
+        <!-- desktop/tablet/laptop View -->
+        <!-- yung magiging default css netong mga to nasa javascrtipt. sundan nalng per ID -->
+        <?php
+
+        foreach ($expenses as $expense) {
+            $description = $expense['description'];
+            $expenseTime = formatDateTime($expense['expenseTime']);
+            $category = $expense['category'];
+            $expenseType = $expense['expenseType'];
+            $amount = $expense['amount'];
+            echo "
+            <div id='large_view' onclick='toggleView()'>
+                <div class='flex justify-between items-center bg-green-800 rounded-t-lg px-4 py-2'>
+                    <h2 class='text-lg  ' id='description'> $description </h2>
+                    <p class='text-sm  ' id='expense_time'> $expenseTime </p>
+                </div>
+                <div class='flex justify-between items-start px-4 py-3 bg-[#03352c] '>
+                    <div> 
+                        <p class='text-sm text-gray-300 pb-2' id='category'> Category: $category </p>
+                        <p class='text-sm text-gray-300' id='expense_type'> Type: $expenseType </p>
+                    </div>
+                    <p class='text-lg font-bold' id='amount'> ₱ $amount </p>
+                </div>
+            </div>";
+        }
+
+        ?>
+
+        <!-- Full View -->
+        <form id="fullView" method="POST" action="" class="hidden">
+            <div class="flex justify-between border-b pb-3 pt-3">
+                <span class="font-semibold text-gray-300">Amount:</span>
+                <input type="text" class="text-gray-300 bg-[#03352c]" id="amount" value="$20" />
+            </div>
+            <div class="flex justify-between border-b pb-3 pt-3">
+                <span class="font-semibold text-gray-300">Category:</span>
+                <input type="text" class="text-gray-300 bg-[#03352c]" id="category" value="dont know yet" />
+            </div>
+            <div class="flex justify-between border-b pb-3 pt-3">
+                <span class="font-semibold text-gray-300">Description:</span>
+                <input type="text" class="text-gray-300 bg-[#03352c]" id="description" value="New York Flight" />
+            </div>
+            <div class="flex justify-between border-b pb-3 pt-3">
+                <span class="font-semibold text-gray-300">Expense Type:</span>
+                <input type="text" class="text-gray-300 bg-[#03352c]" id="expense_type" value="Subscription" />
+            </div>
+            <div class="flex justify-between border-b pb-3 pt-3">
+                <span class="font-semibold text-gray-300">Expense Time:</span>
+                <input type="text" class="text-gray-300 bg-[#03352c]" id="expense_time" value="11 Sep 2001" />
+            </div>
+            <div class="flex justify-start space-x-4 mt-4">
+                <button onclick="saveChanges()" class="bg-blue-500  text-gray-300 px-4 py-1 rounded-lg hover:bg-blue-600">Save Changes</button>
+                <button onclick="deleteExpense()" class="bg-red-500 text-gray-300 px-4 py-1 rounded-lg hover:bg-red-600">Delete</button>
+            </div>
+        </form>
+
+        <script>
+            //global declaration para kunin yung id's nung simplified and fullview
+            const mobileView = document.getElementById("mobile_view");
+            const largeView = document.getElementById("large_view");
+            let fullView = document.getElementById("fullView");
+            let Resolution;
+
+            // eto ung default design class nila. - dineclare ko since - naka- 
+            mobileView.className = " block max-w-md mx-auto bg-[#03352c] shadow-lg rounded-lg p-2 mt-2 cursor-pointer xl:hidden ";
+            largeView.className = " max-w-7xl mx-auto bg-green-900 text-white rounded-lg shadow-lg cursor-pointer";
+
+
+            // eto naman object sababa - eto yung fullview na pagpipilian if mobile size yung resolution or hindi.
+            let fullView_class = {
+
+                mobile: '  lg:hidden xl:hidden max-w-md mx-auto bg-[#03352c] shadow-lg rounded-lg p-5 mt-2 cursor-pointer',
+                NotMobile: '  sm:hidden md:block max-w-7xl mx-auto bg-[#03352c]  lg:block max-w-7xl mx-auto bg-[#03352c] shadow-lg rounded-lg p-5 mt-5 cursor-pointer'
+
+            };
+
+            function checkResolution() {
+
+                Resolution = window.innerWidth; // kukunin neto yung current resolution
+
+                if (Resolution < 768) // if mobile yung resolution
+                {
+                    mobileView.classList.remove("hidden");
+                    largeView.classList.add("hidden");
+                } else {
+                    largeView.classList.remove("hidden");
+                    mobileView.classList.add("hidden");
+                }
+
+                // yung classList.add(""); bali eto yung nagda-dagdag ng value 
+                // sa loob ng isang className
+                fullView.className = 'hidden'; // hahide neto yung fullview by default
+            }
+
+            function toggleView() {
+                Resolution = window.innerWidth; // kukunin neto yung current resolution    
+
+                if (Resolution < 768) {
+                    fullView.className = fullView_class.mobile; // if yung reso pang mobile - then mobile pang mobile yung design                                                         
+                }
+
+                if (Resolution > 767) {
+                    fullView.className = fullView_class.NotMobile; // desktop/tablet/laptop view
+                }
+            }
+
+            function saveChanges() {
+                // pwede na iabang dito yung back-end.
+                // update and retrieved dito.
+
+                fullView.className = 'hidden'; // hahide neto fullview after iclick save changes
+            }
+
+            function deleteExpense() {
+
+                // if unique yung expenseID yun ung gagawin kong reference - geget ko value nya lage
+
+                // after madelete base sa id then loop record ulit
+
+
+                // pero pansamantala - ha-hide mona yung fullview satwing nagde-delete
+                fullView.className = 'hidden'; // hahide neto fullview after iclick save changes
+
+            }
+
+            document.addEventListener('DOMContentLoaded', checkResolution); // eto yung initial load state ng checking ng reso
+            window.addEventListener('resize', checkResolution); // eto naman yung continous checking satwing may changes sa size ng window or webpage
+        </script>
+
+
+    </div>
+
+
+    <?php require('partials/footer.php') ?>
